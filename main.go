2aae018484eb3860c022ce11637e6611576dcf42
linsibo <841773802@qq.com>
2021-01-11 00:05:55 +0800

    add

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
-package godis
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("dfadf")
+	a := 3
+	fmt.Println(a)
+	time.Sleep(time.Second)
+	time.Sleep(time.Second)
 }
